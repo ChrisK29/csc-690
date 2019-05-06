090df7a7cc8afee454d6de179de542891134285a
Alex Sergeev <a_sergeev@outlook.com>
2019-05-05 23:32:39 -0700

    Initial Commit

---
+++ CSC690-Final-Project/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CSC690-Final-Project
+//
+//  Created by Alex Sergeev on 5/5/19.
+//  Copyright © 2019 Alex Sergeev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
